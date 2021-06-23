73000a65deda3a738350a47b9ed916f00cedaabd
bliserge <bliserge34@gmail.com>
2021-06-23 08:05:59 -0700

    Holberton School, 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,35 +1,38 @@
 #include "holberton.h"
-#include "stdio.h"
+
 /**
- * print_to_98 - print numbers in between 
- * @n: integer to start at
- * Return: Void.
+ * print_to_98 - adds two integers.
+ *
+ * Description:Accepts two arguments of type int
+ * and returns the computed addition.
+ *
+ * @nat:first number in parameter list.
+ *
+ * Return: Has a return value of type int.
  */
-void print_to_98
+void print_to_98(int nat)
 {
-int i;
-if (n > 98);
-{
-for (i = n; i >= 98; i--)
-{
-if (i != n)
-{
-printf(",");
-}
-printf("%d", i);
-}
-}
-else
-{
-for (i = n; i <= 98; i++)
-{
-if(i != n)
-{
-printf(", ");
-}
-printf("%d", i);
-}
-}
-putchar('\n');
+	int i, j;
+
+	if (nat <= 98)
+	{
+		for (i = nat; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	}
+	else if (nat >= 98)
+	{
+		for (j = nat; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
 }
 
