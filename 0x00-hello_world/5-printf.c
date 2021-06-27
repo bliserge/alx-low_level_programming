74a73e7d3dcb4d079b9cca39e2fb88d80e4af8f0
bliserge <bliserge34@gmail.com>
2021-06-27 01:38:51 -0700

    Hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+[>77;30407;0c]10;rgb:bfbf/bfbf/bfbf]11;rgb:0000/0000/0000 * main - prints stuff
+ * Return: - 0 for successful return
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
